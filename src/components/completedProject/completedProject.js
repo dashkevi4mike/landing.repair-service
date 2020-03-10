9acc90784e6eb09cb2b3cdbb8e710c9026244c6f
Mikhail <dashkevi4mike@gmail.com>
2020-03-10 23:23:32 +0700

    install slick slider

--- src/components/completedProject/completedProject.js
+++ src/components/completedProject/completedProject.js
@@ -1,2 +1,20 @@
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import $ from 'jquery';
 import './completedProject.scss';
 
+class CompletedProject {
+    constructor() {
+        $('.completed-project__preview').slick({
+            speed: 300,
+            slidesToShow: 2,
+            centerMode: false,
+            variableWidth: true
+        });
+    }
+}
+  
+$(() => {
+    new CompletedProject();
+});
