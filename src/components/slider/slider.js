140a3bfb04f27793c128acc0adf5fb3679f4f871
Mikhail <dashkevi4mike@gmail.com>
2020-03-21 14:39:04 +0700

    slider component

---
+++ src/components/slider/slider.js
@@ -0,0 +1,24 @@
+import 'slick-carousel';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import $ from 'jquery';
+
+import './slider.scss';
+
+class Slider {
+  constructor() {
+    this._attachEventHandler();
+  }
+  
+  _attachEventHandler() {
+    $('.js-slider').slick({
+      speed: 300,
+      centerMode: false,
+      variableWidth: true
+    });
+  }
+}
+  
+$(() => {
+  new Slider();
+});
